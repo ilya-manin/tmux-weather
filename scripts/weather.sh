2b147608cd3a3b699da7a72a7316d65552a3ba8a
Ilya Manin <bkmzvf@gmail.com>
2019-10-18 15:10:12 +0700

    Add a function to get weather

---
+++ scripts/weather.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$CURRENT_DIR/helpers.sh"
+
+get_weather() {
+  local location=$(get_tmux_option "@tmux-weather-location")
+  local format=$(get_tmux_option "@tmux-weather-format" 1)
+
+  curl -s "https://wttr.in/$location?format=$format" | sed "s/[[:space:]]km/km/g"
+}
+
+main() {
+  local update_interval=$((60 * $(get_tmux_option "@tmux-weather-interval" 15)))
+  local current_time=$(date "+%s")
+  local previous_update=$(get_tmux_option "@weather-previous-update-time")
+  local delta=$((current_time - previous_update))
+
+  if [ -z "$previous_update" ]; then
+    echo -n "Please wait..."
+    $(set_tmux_option "@weather-previous-update-time" "$(($current_time - $update_interval))")
+    return 0
+  elif [ $delta -ge $update_interval ]; then
+    local value=$(get_weather)
+    if [ "$?" -eq 0 ]; then
+      $(set_tmux_option "@weather-previous-update-time" "$current_time")
+      $(set_tmux_option "@weather-previous-value" "$value")
+    fi
+  fi
+
+  echo -n $(get_tmux_option "@weather-previous-value")
+}
+
+main
